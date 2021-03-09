3ec32fe11dac07cd4c29b849bc91793f567b3af1
Advithiwhitehat <74882349+Advithiwhitehat@users.noreply.github.com>
2021-03-09 15:35:54 -0800

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,18 @@
+class Ball{
+    constructor(x,y){
+        var options = {
+            density: 1.5,
+            friction: 1.0,
+            restitution: 1.0
+        }
+        this.x = x;
+        this.y = y;
+        this.body = Bodies.circle(this.x, this.y, 10, options);
+    }
+    display(){
+        push();
+        ellipse(10, this.x, this.y);
+        ellipseMode(CENTER);
+        pop();
+    }
+}
